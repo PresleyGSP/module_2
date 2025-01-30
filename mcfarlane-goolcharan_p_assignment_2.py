3abf25abc9e49ae768d90e0fa3d5bef0417ab7c7
Presley McFarlane-Goolcharan <pmcfarlanegoolcharan@rrc.ca>
2025-01-30 14:04:16 -0600

    Third commit: Simple data types added

---
+++ mcfarlane-goolcharan_p_assignment_2.py
@@ -0,0 +1,11 @@
+# SIMPLE DATA TYPES
+
+name ='Presley'
+print("name:", name, "type:", type(name))
+his_license = 'valid'
+print("has license:", his_license, "type:", type(his_license))
+current_year = 2025
+print("this year", current_year, "type:", type(current_year))
+current_year = current_year + 1
+print(current_year)
+print("next_year:", current_year, "type:", type(current_year))
