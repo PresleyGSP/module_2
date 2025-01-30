d36492ed01b40d71462ee464e2b6fe92533dbeb7
Presley McFarlane-Goolcharan <pmcfarlanegoolcharan@rrc.ca>
2025-01-30 14:44:04 -0600

    Fourth commit: added mathematical operations

--- mcfarlane-goolcharan_p_assignment_2.py
+++ mcfarlane-goolcharan_p_assignment_2.py
@@ -9,3 +9,15 @@ print("this year", current_year, "type:", type(current_year))
 current_year = current_year + 1
 print(current_year)
 print("next_year:", current_year, "type:", type(current_year))
+
+
+# MATHEMATICAL OPERATIONS
+
+GST = 0.05
+PST = 0.07
+honda = 7000
+p_tax = 7000 * PST
+g_tax = 7000 * GST
+sum = honda + p_tax + g_tax
+print("Purchase Price:", honda, "Provincial Tax:", PST, "Federal Tax:", GST, "Total:", sum)
+print(f"Purchase Price: ${honda:,.2f} Provincial Tax: ${PST:,.2f} Federal Tax: ${GST:,.2f} Total: ${sum:,.2f} ")
