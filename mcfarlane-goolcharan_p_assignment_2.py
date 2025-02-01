612c0b11a9edc1c2bf795dd55f52c2548be41fd0
Presley McFarlane-Goolcharan <pmcfarlanegoolcharan@rrc.ca>
2025-02-01 17:07:03 -0600

    Twelfth commit: deleted line nine

--- mcfarlane-goolcharan_p_assignment_2.py
+++ mcfarlane-goolcharan_p_assignment_2.py
@@ -6,7 +6,6 @@ print("has license:", his_license, "type:", type(his_license))
 current_year = 2025
 print("this year", current_year, "type:", type(current_year))
 current_year = current_year + 1
-print(current_year)
 print("next_year:", current_year, "type:", type(current_year))
 
 # MATHEMATICAL OPERATIONS
